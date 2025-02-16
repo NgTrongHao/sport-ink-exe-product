26a92574c22022ccad89fee90ce508ee27172290
Nguyen Trong Hao <ngtronghao08@gmail.com>
2025-02-16 22:34:15 +0700

    fix: random string util error

--- sport-ink-exe-software/sport-ink-system-alt/src/main/java/rubberduck/org/sportinksystemalt/shared/common/util/RandomStringUtil.java
+++ sport-ink-exe-software/sport-ink-system-alt/src/main/java/rubberduck/org/sportinksystemalt/shared/common/util/RandomStringUtil.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class RandomStringUtil {
     private static final String ALPHA = "abcdefghijklmnopqrstuvwxyz"; // a-z
+    private static final String DIGITS = "0123456789"; // 0-9
+    private static final String SPECIALS = "~=+%^*/()[]{}/!@#$?|"; // special characters
     private static final String ALPHA_UPPER_CASE = ALPHA.toUpperCase(); // A-Z
     private static final String ALPHA_NUMERIC = ALPHA + ALPHA_UPPER_CASE + DIGITS;
     private static final String ALL = ALPHA_NUMERIC + SPECIALS;
-    private static final String DIGITS = "0123456789"; // 0-9
-    private static final String SPECIALS = "~=+%^*/()[]{}/!@#$?|"; // special characters
     private static final Random generator = new Random();
 
     public static int randomNumber(int min, int max) {
