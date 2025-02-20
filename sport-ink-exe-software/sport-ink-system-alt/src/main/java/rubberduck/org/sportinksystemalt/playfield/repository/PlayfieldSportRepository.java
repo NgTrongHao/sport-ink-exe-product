d484b1bd8c7bff89563a65487b7b14ebff3e94a6
DatVT36 <vuthanhdat181@gmail.com>
2025-02-20 12:04:00 +0700

    feat: remove PlayFieldSportRepo [DatVT]

--- sport-ink-exe-software/sport-ink-system-alt/src/main/java/rubberduck/org/sportinksystemalt/playfield/repository/PlayfieldSportRepository.java
+++
@@ -1,9 +0,0 @@
-package rubberduck.org.sportinksystemalt.playfield.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import rubberduck.org.sportinksystemalt.playfield.domain.entity.PlayfieldSport;
-
-import java.util.UUID;
-
-public interface PlayfieldSportRepository extends JpaRepository<PlayfieldSport, UUID> {
-}
